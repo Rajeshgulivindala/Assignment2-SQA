f0eeed55a7bda170b2bb0f188fe998b7def21ff6
Rajesh <rgulivindala1146@conestoga.on.ca>
2025-03-12 14:44:21 -0400

    Added 4th testcase for Calculation of canvas size i.e.,Testing the func ion when  inputs are decimal values - Failed Test Case

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -5,16 +5,20 @@ describe("calculateCanvasSize", () => {
     const result = calculateCanvasSize("10", "100");
 
     expect(result).toEqual(1000);
-  })
+  });
   test("returns 0 when one of the inputs is 0", () => {
     expect(calculateCanvasSize("0", "100")).toBe(0);
     expect(calculateCanvasSize("100", "0")).toBe(0);
     expect(calculateCanvasSize("0", "0")).toBe(0);
-  })
+  });
   test("handles negative numbers", () => {
     expect(calculateCanvasSize("-10", "10")).toBe(-100);
     expect(calculateCanvasSize("10", "-10")).toBe(-100);
     expect(calculateCanvasSize("-10", "-10")).toBe(100);
+  });
+  test("handles decimal inputs correctly", () => {
+    const result = calculateCanvasSize("10.5", "100.5");
+    expect(result).toEqual(1055.25);
   });
 
 
